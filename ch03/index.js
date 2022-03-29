a66cd3f9f60cb6b14c11df156930fb1188450332
suemor <3100825062@qq.com>
2022-03-29 18:12:50 +0800

    update

---
+++ ch03/index.js
@@ -0,0 +1,21 @@
+;(function doSomething() {
+    "use strict";
+    if (1){
+        var a = 123
+    }
+
+    console.log(a)
+})()
+
+console.log(test.prototype)
+
+function test() {
+
+    message = "hi";
+
+// 全局变量
+
+}
+
+test();
+console.log(message); // "hi"
